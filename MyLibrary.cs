f8349904df81a39f56315705e39d1d6eb5382a9b
ShoshieCoder <46136018+ShoshieCoder@users.noreply.github.com>
2019-03-12 16:08:45 +0200

    Add files via upload

---
+++ MyLibrary.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyLibrary
+    {
+        Dictionary<string, Book> _books;
+
+        public MyLibrary()
+        {
+
+        }
+
+        public bool AddBook(Book b)
+        {
+            if (_books.ContainsKey(b.Title))
+                return false;
+
+            _books.Add(b.Title, b);
+            return true;
+        }
+
+        public bool RemoveBook(string title)
+        {
+            if (!_books.ContainsKey(title))
+                return false;
+
+            _books.Remove(title);
+            return true;
+        }
+
+        public bool HaveThisBook(string title)
+        {
+            if (!_books.ContainsKey(title))
+                return false;
+
+            return true;
+        }
+
+
+        public Book GetBook(string title)
+        {
+            if (!_books.ContainsKey(title))
+                return null;
+
+            return _books[title];
+        }
+
+
+        public Book GetBookByAuthor(string author)
+        {
+            // option 1
+            foreach (KeyValuePair<string, Book> pair in _books)
+            {
+                if (pair.Value.Author == author)
+                    return pair.Value;
+            }
+
+            // option 2
+            foreach (Book book in _books.Values)
+            {
+                if (book.Author == author)
+                    return book;
+            }
+
+            return null;
+        }
+
+        public void Clear()
+        {
+            _books.Clear();
+        }
+
+        public List<string> GetAuthors()
+        {
+            List<string> autors = new List<string>();
+            foreach (Book book in _books.Values)
+            {
+                autors.Add(book.Author);
+            }
+
+            return autors;
+        }
+
+        public List<string> GetBooksTitleSorted()
+        {
+            List<string> titles = new List<string>();
+            foreach (Book book in _books.Values)
+            {
+                titles.Add(book.Title);
+            }
+
+            titles.Sort();
+
+            return titles;
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return _books.Count;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string result = $"My Library: {Count} books \n";
+
+            foreach (KeyValuePair<string, Book> pair in _books)
+            {
+                result += $"[Key: {pair.Key}]\n{pair.Value}\n";
+            }
+
+            return result;
+        }
+
+        public List<Book> GetBooksSortedByAuthorName()
+        {
+            List<Book> books = new List<Book>();
+            foreach (Book book in _books.Values)
+            {
+                books.Add(book);
+            }
+
+            books.Sort();
+
+            return books;
+        }
+    }
+}
